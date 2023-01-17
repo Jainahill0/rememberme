e3fe7b705c8e2b6262e050dd191a917549900627
Jain A. Hill <jainahill700@gmail.com>
2023-01-17 11:17:48 +0530

    My RememberMe app

---
+++ src/components/resource-item/StoredResources.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <resource-item v v-for="res in resources" 
+        :key="res.id" 
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"
+        ></resource-item>
+        <li></li>
+    </ul>
+</template>
+<script>
+import ResourceItem from './ResourceItem.vue';
+export default {
+    inject: ['resources'],
+    components: {
+        ResourceItem
+    }
+};
+</script>
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+  }
+</style>
---
+++ src/components/resource-item/TheResource.vue
@@ -0,0 +1,49 @@
+<template>
+    <base-card>
+       <base-button @click="setSelectedTab('stored-resourses')">Stored Resources</base-button>
+       <base-button @click="setSelectedTab('add-resourse')">Add Resource</base-button>
+
+    </base-card>
+    <component :is="selectedTab"></component>
+</template>
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+    components: {
+        StoredResources,
+        AddResource
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resources',
+            storedResources: [ 
+                {
+                    id: 'official-guide', 
+                    title: 'Official Guide',
+                    description: 'The official Vue.js Documentation.',
+                    link: ''
+             }, 
+                {
+                    id: 'google', 
+                    title: 'Google',
+                    description: 'Learn to google....',
+                    link: ''
+            },
+
+            ],
+        };
+    },
+    provide() {
+        return {
+            resources: this.storedResource
+        };
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+
+</script>
